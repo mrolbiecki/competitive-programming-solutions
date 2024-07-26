58459e290d27dd79e5a1daea97a9881ed1fa2824
Marcin Rolbiecki <rolbieckidev@gmail.com>
2024-07-26 20:26:36 +0200

    rename folders

--- Codeforces/README.txt
+++
@@ -1,3 +0,0 @@
-I've solved more than 200 problems of various difficulty on codeforces.
-My submissions:
-https://codeforces.com/submissions/swm4r
--- IT Camps/Krzyżowa 01.2023/gdz/cgdzlib.h
+++
@@ -1,8 +0,0 @@
-/* Naglowek biblioteki interaktywnej */
-#ifndef CGDZLIB_H
-#define CGDZLIB_H
-int inicjuj();
-int f(int i, int j, int d);
-int g(int i, int j);
-void odpowiedz(int k);
-#endif
